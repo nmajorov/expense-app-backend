c518738040256055a9d8a2e3beb637045c0f956a
Nikolaj Majorov <nikolaj@majorov.biz>
2021-07-06 16:43:48 +0200

    change deployment script

--- tekton-pipelines/deploy_pipeline.sh
+++ tekton-pipelines/deploy_pipeline.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
-echo "delete existing pipelines and tasks first "
+PROJECT=$(oc  project -q)
+
+DIRNAME=`dirname "$0"`
+
+echo "deploy pipelines in project $PROJECT"
+
+
+echo "delete existing pipelines and tasks first if exists in project already"
 
 #CUR_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
@@ -10,12 +17,19 @@ oc delete tasks s2i-quarkus-maven deploy-test-containers backend-mvn deploy-sso 
 
 oc delete pipelines.tekton.dev build-and-deploy-backend --ignore-not-found=true
 
-echo "deploy resources"
-oc create -f resources/
+
+echo "deploy resource workspace pvc"
+oc create -f resources/pvc-workspace.yaml
+
+echo "deploy resource image"
+# need some tricks to set right namespace for image
+cat "$DIRNAME/resources/image-resource.yaml" | sed -e "s/nm-demo/$PROJECT/g" | oc create -f -
+
 
 echo "deploy tasks"
 oc create -f tasks/
 
 echo "deploy pipeline"
 oc create -f pipeline.yaml
+
 
