6e447a7e9c6c020c3c92180804621fda3320a3fd
Nikolaj Majorov <nikolaj@majorov.biz>
2022-08-16 11:01:52 +0200

    add test for oidc endpoint
    add test for oidc endpoint

--- test_oidc_endpoint.sh
+++ test_oidc_endpoint.sh
@@ -1,25 +1,32 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 echo "emulate react app call to keycloak"
 
 USER_NAME=niko
-PASSWORD=niko
+PASSWORD=openshift
+
+SSO_REALM_URL="http://localhost:7080/auth/realms/basic"
+
+if [ "X$1" == "X" ] ; then
+     echo
+else 
+     SSO_REALM_URL=$1
+fi
+
+
+echo "using ream url: ${SSO_REALM_URL}"
 
 export access_token=$(\
 
-    curl -v  -X POST "http://localhost:7080/auth/realms/basic/protocol/openid-connect/token" \
+    curl -kv "${SSO_REALM_URL}/protocol/openid-connect/token" \
     -H 'content-type: application/x-www-form-urlencoded' \
     -d "grant_type=password&username=$USER_NAME&password=$PASSWORD&client_id=app-react" | jq --raw-output '.access_token' \
 )
 
 echo "access_token: $access_token"
 
-curl -v -X GET http://localhost:3000/api/reports -H "Authorization: Bearer "$access_token
-
-exit 0
 
 curl -v -X GET \
-  http://localhost:8080/reports \
-  -H "Authorization: Bearer "$access_token
+  http://localhost:8080/reports -H "Authorization: Bearer ${access_token}"
 
 
